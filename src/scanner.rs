b9dc6d3d7a1b7dbb36e9de1a3662c8cec6938551
Martin Pool <mbp@sourcefrog.net>
2021-09-29 11:30:15 -0700

    Factor out a Peek class

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,9 +2,7 @@
 
 //! Scan text to tokens.
 
-use std::iter::Peekable;
-
-use crate::Result;
+// use crate::Result;
 
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
@@ -13,10 +11,9 @@ pub enum TokenType {
     Star,
 
     // Literals
-    String(String),
+    // String(String),
     Number(f64),
-    Identifier(String),
-
+    // Identifier(String),
     Eof,
 }
 
@@ -31,14 +28,14 @@ pub struct Token {
 pub fn scan<'s>(source: &'s str) -> impl Iterator<Item = Token> + 's {
     Scanner {
         // source,
-        chars: source.chars().peekable(),
+        chars: Peek::new(source.chars()),
         line: 1,
         past_eof: false,
     }
 }
 
 struct Scanner<'s> {
-    chars: Peekable<std::str::Chars<'s>>,
+    chars: Peek<char, std::str::Chars<'s>>,
     // source: &'s str,
     line: usize,
     past_eof: bool,
@@ -51,42 +48,42 @@ impl<'s> Iterator for Scanner<'s> {
         loop {
             if self.past_eof {
                 return None;
-            } else if let Some(ch) = self.chars.next() {
-                let token_type = match ch {
-                    '\n' => {
-                        self.line += 1;
-                        continue;
-                    }
-                    ' ' | '\t' | '\r' => {
-                        continue;
-                    }
-                    '+' => TokenType::Plus,
-                    '*' => TokenType::Star,
-                    '-' => TokenType::Minus,
-                    '0'..='9' => {
-                        let mut s = String::new();
-                        s.push(ch);
-                        while let Some(cc) = self.take_if(|c| c.is_ascii_digit()) {
-                            s.push(cc)
-                        }
-                        if self.take_if(|c| c == '.').is_some() {
-                            s.push('.');
-                            while let Some(cc) = self.take_if(|c| c.is_ascii_digit()) {
-                                s.push(cc)
-                            }
-                        }
-                        let val: f64 = s.parse().unwrap();
-                        TokenType::Number(val)
-                    }
-                    other => panic!("unhandled character {:?}", other),
-                };
-                return self.emit(token_type);
-            } else {
+            } else if self.chars.is_empty() {
                 // Return one Eof and then stop the iterator.
                 // (Maybe it should just stop?)
                 self.past_eof = true;
                 return self.emit(TokenType::Eof);
             }
+            let ch = self.chars.take().unwrap();
+            let token_type = match ch {
+                '\n' => {
+                    self.line += 1;
+                    continue;
+                }
+                ' ' | '\t' | '\r' => {
+                    continue;
+                }
+                '+' => TokenType::Plus,
+                '*' => TokenType::Star,
+                '-' => TokenType::Minus,
+                '0'..='9' => {
+                    let mut s = String::new();
+                    s.push(ch);
+                    while let Some(cc) = self.chars.take_if(|c| c.is_ascii_digit()) {
+                        s.push(cc)
+                    }
+                    if self.chars.take_if(|&c| c == '.').is_some() {
+                        s.push('.');
+                        while let Some(cc) = self.chars.take_if(|c| c.is_ascii_digit()) {
+                            s.push(cc)
+                        }
+                    }
+                    let val: f64 = s.parse().unwrap();
+                    TokenType::Number(val)
+                }
+                other => panic!("unhandled character {:?}", other),
+            };
+            return self.emit(token_type);
         }
     }
 }
@@ -98,22 +95,69 @@ impl<'s> Scanner<'s> {
             line: self.line,
         })
     }
+}
 
-    fn take_if<F>(&mut self, f: F) -> Option<char>
+/// Iterator adapter allowing arbitrary-length peeking ahead.
+///
+/// Beyond [std::iter::Peekable] this allows looking more than one
+/// item ahead.
+struct Peek<C, I>
+where
+    I: Iterator<Item = C>,
+{
+    inner: I,
+    buf: Vec<C>,
+}
+
+impl<C, I> Peek<C, I>
+where
+    I: Iterator<Item = C>,
+{
+    fn new(inner: I) -> Peek<C, I> {
+        Peek {
+            inner,
+            buf: Vec::new(),
+        }
+    }
+
+    fn take(&mut self) -> Option<C> {
+        if self.buf.is_empty() {
+            self.inner.next()
+        } else {
+            Some(self.buf.remove(0))
+        }
+    }
+
+    fn take_if<F>(&mut self, f: F) -> Option<C>
     where
-        F: FnOnce(char) -> bool,
+        F: FnOnce(&C) -> bool,
     {
-        match self.chars.peek() {
+        match self.peek() {
             None => None,
-            Some(&c) => {
+            Some(c) => {
                 if f(c) {
-                    self.chars.next();
-                    Some(c)
+                    self.take()
                 } else {
                     None
                 }
             }
         }
+    }
+
+    fn peek(&mut self) -> Option<&C> {
+        if self.buf.is_empty() {
+            if let Some(c) = self.inner.next() {
+                self.buf.push(c)
+            } else {
+                return None;
+            }
+        }
+        Some(&self.buf[0])
+    }
+
+    #[allow(unused)]
+    fn is_empty(&mut self) -> bool {
+        self.peek().is_some()
     }
 }
 
